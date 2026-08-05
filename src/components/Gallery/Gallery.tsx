32104135c98e62d72bd60a6cecca23eec106f192
Lu Lopez <lucianaalopez97@gmail.com>
2026-08-04 22:36:21 -0300

    tiempo foto phone

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -90,7 +90,7 @@ export function Gallery() {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              transition={{ duration: 0.4, ease: 'easeInOut' }}
+              transition={{ duration: 1, ease: 'easeInOut' }}
             >
               <img
                 src={activeImage.src}
